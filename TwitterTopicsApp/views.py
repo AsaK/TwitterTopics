058a671b1b37afa0777a93ebe4acdb048189b01d
Mateus Roberto <mateus.roberto@rh3software.local>
2016-03-09 16:11:16 -0300

    git commit -m

--- TwitterTopicsApp/views.py
+++ TwitterTopicsApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from requests_oauthlib import OAuth1Session
+
 import json
 # Create your views here.
 def index(request):
@@ -10,17 +11,4 @@ def index(request):
     access_token_secret = ""
     session = OAuth1Session(api_key, api_secret, access_token, access_token_secret)
     response = session.get("https://api.twitter.com/1.1/trends/place.json?id=1")
-    worlds = json.loads(response.content)[0]["trends"]
-    context = {'tt1': worlds[0]["name"],
-               'tt2': worlds[1]["name"],
-               'tt3': worlds[2]["name"],
-               'tt4': worlds[3]["name"],
-               'tt5': worlds[4]["name"],
-               'tt6': worlds[5]["name"],
-               'tt7': worlds[6]["name"],
-               'tt8': worlds[7]["name"],
-               'tt9': worlds[8]["name"],
-               'tt10': worlds[9]["name"],
-               }
-
-    return render(request, 'TwitterTopicsApp/index.html', context)
+    return render(request, 'TwitterTopicsApp/index.html', response)
